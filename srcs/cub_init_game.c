85bd021017be83afea2f9cc704b2433ad2ddc25f
Clement Sapt <csapt@student.42lyon.fr>
2021-01-02 16:19:19 +0100

    fix double func :hammer:

--- srcs/cub_init_game.c
+++ srcs/cub_init_game.c
@@ -6,7 +6,7 @@
 /*   By: csapt <csapt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 21:35:03 by csapt             #+#    #+#             */
-/*   Updated: 2021/01/02 15:54:53 by csapt            ###   ########lyon.fr   */
+/*   Updated: 2021/01/02 16:13:20 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,6 @@ void	init_bonus(t_global *env)
 		error_cub("Allocation", env);
 	env->op.menu = true;
 	mlx_mouse_hide();
-}
-
-int		create_xpm_bonus(t_global *env)
-{
-	if (!(env->main->menu = create_tab_xpm(env->win.mlx, 4, env->data.tex)))
-		return (1);
-	if (!(env->main->resume = create_tab_xpm(env->win.mlx, 5,
-	env->data.resume)))
-		return (1);
-	if (!(env->main->settings = create_tab_xpm(env->win.mlx, 2,
-	env->data.settings)))
-		return (1);
-	if (!(env->main->cur->img = create_xpm_image(env->win.mlx,
-	"assets/ui/cursor/cursor.xpm")))
-		return (1);
-	if (!(env->main->map = create_xpm_image(env->win.mlx,
-	"assets/ui/map/cardinal-point.xpm")))
-		return (1);
-	if (!(env->main->on = create_xpm_image(env->win.mlx,
-	"assets/ui/settings/whitepixel.xpm")))
-		return (1);
-	return (0);
 }
 
 void	init_game(t_global *env)
